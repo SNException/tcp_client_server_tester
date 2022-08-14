234d4f0fd036f6e360bf61570d8568cf8e117cd1
SNException <thexsalkin@gmail.com>
2022-08-14 10:45:48 +0200

    settings are now more flexible

--- src/SettingsWindow.java
+++ src/SettingsWindow.java
@@ -17,7 +17,9 @@
 //
 
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.event.*;
 import javax.swing.table.*;
 
 public final class SettingsWindow {
@@ -42,30 +44,27 @@ public final class SettingsWindow {
         final SettingsTableModel model = new SettingsTableModel();
 
         insert_settings: {
-            model.addRow(new Object[]{"Wrap STX-ETX",    Settings.wrapInStxEtx});
-            model.addRow(new Object[]{"Insert new line", Settings.insertNewLine});
+            model.addRow(new Object[]{"Wrap STX-ETX",    String.valueOf(Settings.wrapInStxEtx)});
+            model.addRow(new Object[]{"Insert new line", String.valueOf(Settings.insertNewLine)});
         }
 
         final JTable table = new JTable(model);
+        table.putClientProperty("terminateEditOnFocusLost", true);
         table.getTableHeader().setReorderingAllowed(false);
         root.add(new JScrollPane(table), BorderLayout.CENTER);
 
         final JButton saveButton = new JButton("Save");
         saveButton.addActionListener(e -> {
-            final boolean stxetx  = (boolean) model.getValueAt(0, 1);
-            final boolean newline = (boolean) model.getValueAt(1, 1);
+            // note(nschultz): Already validated
+            final boolean stxetx  = Boolean.parseBoolean((String) model.getValueAt(0, 1));
+            final boolean newline = Boolean.parseBoolean((String) model.getValueAt(1, 1));
 
             Settings.wrapInStxEtx  = stxetx;
             Settings.insertNewLine = newline;
 
             this.frame.dispose();
-            /*for (int x = 0; x < table.getColumnCount(); ++x) {
-                for (int y = 0; y < table.getRowCount(); ++y) {
-                    System.err.println(model.getValueAt(x, y));
-                }
-            }*/
         });
-        // saveButton.setPreferredSize(new Dimension(100, 20));
+
         root.add(saveButton, BorderLayout.SOUTH);
 
         this.frame.setContentPane(root);
@@ -84,18 +83,45 @@ public final class SettingsWindow {
             super(new String[] {"Name", "Value"}, 0);
         }
 
-        @Override
+        // note(nschultz): I would love to do that. However, I don't think this works
+        // if I want to have different types of components in the same column.
+        // So for example I would like some rows to have a checkbox, and others
+        // a combobox. But that is not possible with that solution. Since I can only generate one type of component
+        // for all rows in a column.
+        // Therefore I will have to use textfields for all of them, which is not ideal, because we are now
+        // prone to bad user input. *Sigh*
+        /*@Override
         public Class<?> getColumnClass(final int columnIndex) {
             return switch (columnIndex) {
                 case 0  -> String.class;
                 case 1  -> Boolean.class;
                 default -> String.class;
             };
-        }
+        }*/
 
         @Override
         public boolean isCellEditable(final int row, final int column) {
             return column == 1;
+        }
+
+        @Override
+        public void setValueAt(final Object value, final int row, final int column) {
+            super.setValueAt(value, row, column);
+
+            // note(nschultz): validation
+            final String stxetx = ((String) super.getValueAt(0, 1)).strip();
+            if (stxetx.equalsIgnoreCase("true") || stxetx.equalsIgnoreCase("false")) {
+                // note(nschultz): we good
+            } else {
+                super.setValueAt("false", 0, 1);
+            }
+
+            final String newline = ((String) super.getValueAt(1, 1)).strip();
+            if (newline.equalsIgnoreCase("true") || newline.equalsIgnoreCase("false")) {
+                // note(nschultz): we good
+            } else {
+                super.setValueAt("false", 1, 1);
+            }
         }
     }
 }
